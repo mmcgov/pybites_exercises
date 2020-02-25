dc4c521692b85d429188268a07aeede4a33cf72b
Martin McGovern <martin.mcgovern@aon.com>
2020-02-25 13:44:53 +0000

    answer for 251

--- 251/series.py
+++ 251/series.py
@@ -34,7 +34,8 @@ def object_values_series() -> pd.Series:
     so index 101='A', 102='B' ... 125='Y', 126='Z'
     Don't worry about the series name.
     """
-    ser = pd.Series([str(letter) for letter in string.ascii_uppercase],
-                     index = [np.linspace(start = 101,stop = 126,num = 26, dtype = int)])
+    num_ind = 
+    ser = pd.Series([letter for letter in string.ascii_uppercase],
+                     index = np.linspace(start = 101,stop = 126,num = 26, dtype = int))
     return ser
 
