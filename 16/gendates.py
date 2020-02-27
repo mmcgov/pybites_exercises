4e0961c8399b5b4f290ce869ab3b581795a9375d
Martin McGovern <martin.mcgovern@aon.com>
2020-02-27 09:54:26 +0000

    sol for 16

---
+++ 16/gendates.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+PYBITES_BORN = datetime(year=2016, month=12, day=19)
+
+
+def gen_special_pybites_dates():
+    final_dates = list()
+    for i in range(0, 11):
+        if i%4==0:
+            factor = i/4
+            yield PYBITES_BORN + timedelta(days=365*factor)
+        else:
+            yield PYBITES_BORN + timedelta(days=100*i)
+
