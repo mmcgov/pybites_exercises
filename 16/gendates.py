cb1283a3a26d632c529ad7dc0c953775f74f8db0
Martin McGovern <martin.mcgovern@aon.com>
2020-02-27 10:10:32 +0000

    sol for 16

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,11 +4,8 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    for i in range(1, 9):
-        if i % 4 == 0:
-            factor = i/4
-            yield PYBITES_BORN + timedelta(days=365*factor)
-            yield PYBITES_BORN + timedelta(days=100*i)
-        else:
-            yield PYBITES_BORN + timedelta(days=100*i)
-
+    days = 0
+    while True:
+        days += 1
+        if days % 100 == 0 or days % 365 == 0:
+            yield PYBITES_BORN + timedelta(days=days)
