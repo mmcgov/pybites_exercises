689c020f3f97e395c00e3c4c8ec7f5f4e9dd0c28
Martin McGovern <martin.mcgovern@aon.com>
2020-03-02 11:08:05 +0000

    sol for 44

---
+++ 44/license.py
@@ -0,0 +1,12 @@
+import secrets
+import string
+
+def gen_key(*, parts=4, chars_per_part=8):
+    alphabet = string.ascii_uppercase + string.digits
+    keys = list()
+    for part in range(parts):
+        keys.append(''.join(secrets.choice(alphabet) for i in range(chars_per_part)))
+    return '-'.join(keys)
+
+    
+
