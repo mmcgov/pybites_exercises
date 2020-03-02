d66f05b310170dd703369d8f2366474d15dbf026
Martin McGovern <martin.mcgovern@aon.com>
2020-03-02 11:11:33 +0000

    sol for 44

--- 44/license.py
+++ 44/license.py
@@ -6,6 +6,6 @@ def gen_key(*, parts=4, chars_per_part=8):
     alphabet = string.ascii_uppercase + string.digits
     keys = list()
     for part in range(parts):
-        keys.append(''.join(secrets.choice(alphabet) \
+        keys.append(''.join(secrets.choice(alphabet)
                     for i in range(chars_per_part)))
     return '-'.join(keys)
