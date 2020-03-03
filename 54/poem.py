9323f5cbd7b8477eb9a78c64630af20455f3e523
Martin McGovern <martin.mcgovern@aon.com>
2020-03-03 15:31:30 +0000

    sol for 54

--- 54/poem.py
+++ 54/poem.py
@@ -8,9 +8,9 @@ def print_hanging_indents(poem):
         line = line.strip()
         if len(line) == 0:
             indent_check = 0
-        if (len(line) > 0) and (indent_check == 0):
+        elif (len(line) > 0) and (indent_check == 0):
             print(line)
             indent_check = 1
-        if (len(line) > 0) and (indent_check == 1):
+        elif (len(line) > 0) and (indent_check == 1):
             print(' '*4, line)
             indent_check = 1
