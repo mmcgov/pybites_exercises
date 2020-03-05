715fb1cde887b59c05bd6409ae585bbfa1a99e14
Martin McGovern <martin.mcgovern@aon.com>
2020-03-05 15:06:34 +0000

    sol for 66

---
+++ 66/running_mean.py
@@ -0,0 +1,8 @@
+import itertools
+
+def running_mean(sequence):
+    """Calculate the running mean of the sequence passed in,
+       returns a sequence of same length with the averages.
+       You can assume all items in sequence are numeric."""
+    sums = itertools.accumulate(sequence)
+    yield [round(accum/i,2) for i, accum in enumerate(sums, start=1)]
