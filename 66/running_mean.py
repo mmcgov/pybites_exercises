d287efe213e59dd3ec5fd5d08ddbc1c027281af3
Martin McGovern <martin.mcgovern@aon.com>
2020-03-05 15:27:32 +0000

    sol for 66

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -5,4 +5,12 @@ def running_mean(sequence):
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
     sums = itertools.accumulate(sequence)
-    yield [int(accum/i) if float(accum/i).is_integer() else round(accum/i, 2)  for i, accum in enumerate(sums, start=1)]
+    for i, accum in enumerate(sums, start=1):
+        if float(accum/i).is_integer():
+            yield int(accum/i)
+        else:
+            yield round(accum/i, 2)
+
+
+
+   # yield int(accum/i) if float(accum/i).is_integer() else round(accum/i, 2)  for i, accum in enumerate(sums, start=1)
