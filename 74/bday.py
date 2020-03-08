7867ef1688d2f9c60e336af6d8e77bd8d9ebe5bd
mmcgov <mmcgov@outlook.com>
2020-03-08 16:45:29 +0000

    sol for 74

---
+++ 74/bday.py
@@ -0,0 +1,7 @@
+from datetime import date
+import calendar
+
+
+def weekday_of_birth_date(date):
+    """Takes a date object and returns the corresponding weekday string"""
+    return calendar.day_name[date.weekday()]
