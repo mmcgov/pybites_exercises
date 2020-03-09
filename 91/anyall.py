08e2899d68e64f68bde893d9979e586a8b2846bc
Martin McGovern <martin.mcgovern@aon.com>
2020-03-09 14:41:19 +0000

    sol for 91

--- 91/anyall.py
+++ 91/anyall.py
@@ -5,14 +5,15 @@ PYTHON = 'python'
 def contains_only_vowels(input_str):
     """Receives input string and checks if all chars are
        VOWELS. Match is case insensitive."""
-    result = ''.join([letter for letter in input_str if letter.lower() in VOWELS])
+    result = ''.join([letter for letter in input_str 
+                      if letter.lower() in VOWELS])
     return result == input_str
 
 
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    return any(i.lower() in input_str for i in PYTHON)
+    return any(i in input_str.lower() for i in PYTHON)
 
 
 def contains_digits(input_str):
