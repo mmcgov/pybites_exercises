fa2c39319e218ff612d6699851089041035b9acb
Martin McGovern <martin.mcgovern@aon.com>
2020-03-11 15:38:09 +0000

    sol for 153

---
+++ 153/rounding.py
@@ -0,0 +1,8 @@
+import math
+
+def round_up_or_down(transactions, up=True):
+    """Round the list of transactions passed in.
+       If up=True (default) ronud up, else round down.
+       Return a new list of rounded values
+    """
+    return [math.ceil(t) if up == True else math.floor(t) for t in transactions]
