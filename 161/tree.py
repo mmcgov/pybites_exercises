47b34c695557b881b94351ec6054428cbe597250
Martin McGovern <martin.mcgovern@aon.com>
2020-03-13 10:26:54 +0000

    sol for 161

--- 161/tree.py
+++ 161/tree.py
@@ -5,5 +5,5 @@ def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    return (len(os.walk(path).next()[1]), len(os.walk(path).next()[2]))
+    return (sum((len(f) for _, f, _ in os.walk(myPath))), sum((len(f) for _, _, f in os.walk(myPath))))
 
