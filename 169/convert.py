aa56ba1466a1b7ea8df36f10a4d0a24449c17ba4
mmcgov <mmcgov@outlook.com>
2020-03-15 00:44:40 +0000

    sol for 169

--- 169/convert.py
+++ 169/convert.py
@@ -5,7 +5,6 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    cmtoinch = 0.3937
     inchtocm = 2.54
 
     if isinstance(value, int) or isinstance(value, float):
@@ -13,11 +12,9 @@ def convert(value: float, fmt: str) -> float:
     else:
         raise TypeError
 
-    if fmt.contains('cm'):
+    if 'cm' in fmt.lower():
         return round(value*inchtocm, 4)
-    elif fmt.contains('in'):
-        return round(value*cmtoinch, 4)
+    elif 'in' in fmt.lower():
+        return round(value/inchtocm, 4)
     else:
         raise ValueError
-
-
