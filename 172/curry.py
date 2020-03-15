24457581897923c2c2e8def9fe9aeb23a89d7656
mmcgov <mmcgov@outlook.com>
2020-03-15 12:05:00 +0000

    sol for 172

--- 172/curry.py
+++ 172/curry.py
@@ -3,11 +3,6 @@ from functools import partial
 # create 2 partials:
 # - 'rounder_int' rounds to int (0 places)
 # - 'rounder_detailed' rounds to 4 places
+rounder_int = partial(round, ndigits=0)
+rounder_detailed = partial(round, ndigits=4)
 
-
-def round_new(number, places):
-    return round(number, places)
-
-
-rounder_int = partial(round_new, places=0)
-rounder_detailed = partial(round_new, places=4)
