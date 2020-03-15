06f9e81e072df5dc2c33bbef4b0d7f9b45562d8f
mmcgov <mmcgov@outlook.com>
2020-03-15 22:29:11 +0000

    sol for 180

--- 180/names.py
+++ 180/names.py
@@ -18,5 +18,5 @@ def group_names_by_country(data: str = data) -> defaultdict:
     countries = defaultdict(list)
     for line in data.split('\n')[1:]:
         items = line.split(',')
-        countries[items[2]].append(' '.join(items[:2]))
+        countries[items[2]].append(items[0]+' '+items[1])
     return countries
