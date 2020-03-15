d33504443699e4e35570c2309013c87e3440ba81
mmcgov <mmcgov@outlook.com>
2020-03-15 12:46:27 +0000

    sol for 176

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -6,10 +6,12 @@ def create_chessboard(size=8):
        Don't return anything, print the output to stdout"""
     odd = 0
     length = int(size/2)
-    for i in range(size+1):
+    for i in range(size):
         if odd == 0:
             print((WHITE+BLACK)*length)
             odd = 1
+            continue
         if odd == 1:
             print((BLACK+WHITE)*length)
             odd = 0
+            continue
