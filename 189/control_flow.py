694ab4da43a7a36abff8d04a1239dd1bcb03b35f
mmcgov <mmcgov@outlook.com>
2020-03-16 22:29:22 +0000

    sol for 189

---
+++ 189/control_flow.py
@@ -0,0 +1,15 @@
+IGNORE_CHAR = 'b'
+QUIT_CHAR = 'q'
+MAX_NAMES = 5
+
+
+def filter_names(names):
+    results = list()
+    for name in names:
+        if name[0] == IGNORE_CHAR:
+            continue
+        elif name[0] == QUIT_CHAR:
+            break
+        elif len(results) <= MAX_NAMES:
+            results.append(name)
+
