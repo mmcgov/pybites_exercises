3c35bd44de23120d21c0424e0ba9a4ea2b1d211a
mmcgov <mmcgov@outlook.com>
2020-03-20 01:03:02 +0000

    sol for 225

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -5,9 +5,13 @@ def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
        Return the resulting string."""
  #   return [c.upper() if c.islower() else c.lower() for c in text if c.lower() in PYBITES]
+    new =""
     for char in text:
         if (char.lower() in PYBITES) and (char.islower()):
+            new+=char.upper()
             text = text.replace(char, char.upper())
         elif (char.lower() in PYBITES) and (char.isupper()):
-            text = text.replace(char, char.lower())
-    return text
+            new+=char.lower()
+        else:
+            new+=char
+    return new
