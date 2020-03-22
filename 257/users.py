84a7cb43c2d1481dade9dfc9cbb89c868cc5a86d
mmcgov <mmcgov@outlook.com>
2020-03-22 12:25:10 +0000

    sol for 257

--- 257/users.py
+++ 257/users.py
@@ -1,16 +1,22 @@
+import re
+
+pattern = re.compile(r'(,){2,}')
+
+
 def get_users(passwd: str) -> dict:
     """Split password output by newline,
-      extract user and name (1st and 5th columns),
+ mport      extract user and name (1st and 5th columns),
       strip trailing commas from name,
       replace multiple commas in name with a single space
       return dict of keys = user, values = name.
     """
     results = dict()
-    passwd = passwd.split('\n')[1:-1]
+    passwd = re.sub(pattern, ' ', passwd).split('\n')[1:-1]
     for entry in passwd:
         entry = entry.split(':')
         results[entry[0].strip()] = entry[4].replace(',','').strip() \
                                     if entry[4] else 'unknown'
     return results
+
 
 
